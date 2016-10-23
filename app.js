21c2f9787fe397a899897844876cd419d7f3d1ee
Ayush Bajpai <Ayush Bajpai>
2016-10-23 10:26:37 +0530

    First Angular app

---
+++ app.js
@@ -0,0 +1,10 @@
+(function(){
+  angular.module('myFirstApp',[])//this has been bound by angular to html tag using ng-app attribute
+
+  .controller("myFirstController",function(){
+    
+  });
+})();//iife -imediately invoked function expression
+// only variable that angular exposes globally is angular.
+//[] in module contains dependencies in the project.
+//
